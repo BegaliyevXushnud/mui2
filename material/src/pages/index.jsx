8c92a5cd5a90bf62a7c5cb1a7c59a135defdeea1
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-09-11 10:11:07 +0500

    Material papkasini o'chirdim

--- material/src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-
-import SignIn from "./sign-in"
-import AdminLayout from './admin-layout'
-import StudentLayout from './student-layout'
-import Teacher from  "./teacher"
-import Student from "./student"
-export {SignIn,AdminLayout,StudentLayout,Student,Teacher}
